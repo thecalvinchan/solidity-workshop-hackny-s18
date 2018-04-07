03068a0b75de9fcaf4507128a5768b4fcaecc8a9
Calvin Chan <calvin.chan.h@gmail.com>
2018-04-07 15:10:23 -0400

    Added rinkeby network

--- truffle.js
+++ truffle.js
@@ -1,11 +1,23 @@
+const dotenv = require('dotenv')
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+dotenv.config()
+
+const mnemonic = process.env.MNEMONIC
+const apiKey = process.env.API_KEY
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 9545,
-      network_id: "*"
+      network_id: '*'
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/'+apiKey),
+      network_id: 3
     }
   }
 };
